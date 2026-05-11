56eafbd0d4e16a5cfecbd3a7196ce6e81576ef83
JustJaimii <jancarlodalacat@gmail.com>
2026-05-11 09:05:37 +0800

    Initial commit

---
+++ src/main/kotlin/project/kompass/fancyTrees/FancyTrees.kt
@@ -0,0 +1,14 @@
+package project.kompass.fancyTrees
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FancyTrees : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
